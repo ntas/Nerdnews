0c4b2d611c33a9b586cc08b4289e7ac5b6c97251
Tim Littlemore <Tim@iMac.local>
2009-09-30 21:12:06 +0100

    Installed annotate_models plugin Ran annotate_models rake task

--- app/models/rss_item.rb
+++ app/models/rss_item.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+# Schema version: 20090926230647
+#
+# Table name: rss_items
+#
+#  id          :integer         not null, primary key
+#  source      :string(255)
+#  url         :string(255)
+#  title       :string(255)
+#  description :text
+#  post_time   :datetime
+#
+
 class RssItem < ActiveRecord::Base
 end
