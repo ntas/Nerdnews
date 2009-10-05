d49ccf7d80797ae375df871fba844d42defdb12e
Tim Littlemore <Tim@iMac.local>
2009-10-05 22:31:10 +0100

    Various CSS and layout changes. Ran capify

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Detect iphone/ipod touch requests
+  def iphone_request?
+    (agent = request.env["HTTP_USER_AGENT"]) && agent[/(Mobile\/.+Safari)/]
+  end
 end
