7dfe1e4a5b2abdaebd185466ca745f4089d8a8a4
Tim Littlemore <tim@tjl2.com>
2010-06-18 21:32:32 +0100

    Remove new_rails_defaults.rb, fixed routes file to revert to previous behaviour and removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Nerdnews::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "rss_items#index"
 
   # See how all your routes lay out with "rake routes"
 
