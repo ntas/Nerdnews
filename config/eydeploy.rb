7eb9c094e30e57c72c946901566e5c9804d3c90b
Tim Littlemore <tim@tjl2.com>
2011-09-13 16:05:17 +0100

    Trying out overriding bundle part of deploy

---
+++ config/eydeploy.rb
@@ -0,0 +1,42 @@
+def bundle
+
+  extra_excluded_groups = ['cucumber']
+
+  if File.exist?("#{c.release_path}/Gemfile")
+    info "~> Gemfile detected, bundling gems (customised)"
+    lockfile = File.join(c.release_path, "Gemfile.lock")
+
+    bundler_installer = if File.exist?(lockfile)
+                          get_bundler_installer(lockfile, " #{extra_excluded_groups.join(' ')}")
+                        else
+                          missing_lock_version = EY::Serverside::LockfileParser::Parse10::DEFAULT
+                          warn_about_missing_lockfile missing_lock_version
+                          bundler_10_installer missing_lock_version
+                        end
+
+    sudo "#{serverside_bin} _#{EY::Serverside::VERSION}_ install_bundler #{bundler_installer.version}"
+
+    bundled_gems_path = File.join(c.shared_path, "bundled_gems")
+    ruby_version_file = File.join(bundled_gems_path, "RUBY_VERSION")
+    system_version_file = File.join(bundled_gems_path, "SYSTEM_VERSION")
+    ruby_version = `ruby -v`
+    system_version = `uname -m`
+
+    if File.directory?(bundled_gems_path)
+      rebundle = false
+
+      rebundle = true if File.exist?(ruby_version_file) && File.read(ruby_version_file) != ruby_version
+      rebundle = true if File.exist?(system_version_file) && File.read(system_version_file) != system_version
+
+      if rebundle
+        info "~> Ruby version change detected, cleaning bundled gems"
+        run "rm -Rf #{bundled_gems_path}"
+      end
+    end
+
+    run "cd #{c.release_path} && bundle _#{bundler_installer.version}_ install #{bundler_installer.options}"
+
+    run "mkdir -p #{bundled_gems_path} && ruby -v > #{ruby_version_file} && uname -m > #{system_version_file}"
+  end
+end
+
